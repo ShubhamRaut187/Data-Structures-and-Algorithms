e199e8cfc9c34d6555fe966a72d60237b0448cc4
ShubhamRaut187 <raut.shubham4554@gmail.com>
2024-01-25 13:06:31 +0530

    ThirdMaxNumber

---
+++ Leet Code/Arrays/ThirdMaxNumber.java
@@ -0,0 +1,58 @@
+// Given an integer array nums, return the third distinct maximum number in this array. If the third maximum does not exist, return the maximum number.
+
+// Example 1:
+// Input: nums = [3,2,1]
+// Output: 1
+// Explanation:
+// The first distinct maximum is 3.
+// The second distinct maximum is 2.
+// The third distinct maximum is 1.
+
+// Example 2:
+// Input: nums = [1,2]
+// Output: 2
+// Explanation:
+// The first distinct maximum is 2.
+// The second distinct maximum is 1.
+// The third distinct maximum does not exist, so the maximum (2) is returned instead.
+
+// Example 3:
+// Input: nums = [2,2,3,1]
+// Output: 1
+// Explanation:
+// The first distinct maximum is 3.
+// The second distinct maximum is 2 
+// (both 2s are counted together since they have the same value).
+// The third distinct maximum is 1.
+ 
+
+// Constraints:
+// 1 <= nums.length <= 104
+// -231 <= nums[i] <= 231 - 1
+
+// Solution
+
+import java.util.*;
+class ThirdMaxNumber{
+    public static int ThirdMax(int[] nums, int N){
+    //    Time Complexity = O(n*log(n)).
+       Arrays.sort(nums);
+       if(N <= 2){
+           return nums[N-1];
+       }
+       else{
+           return nums[0];
+       }
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Length of Array: ");
+        int N = sc.nextInt();
+        System.out.println("Enter Array Elements: ");
+        int [] arr = new int[N];
+        for(int i=0;i<N;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Answer is: "+ThirdMax(arr,N));
+    }
+}
